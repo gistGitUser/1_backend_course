d8c41437b193903be0047f88b422bd3f834c36e7
FIRST_NAME LAST_NAME <gistuser@bk.ru>
2023-09-30 13:34:18 +0300

    lesson 13 and test api

--- makefile
+++ makefile
@@ -1,5 +1,3 @@
-
-
 postgres:
 	docker run --name backend_corse_postgres -e POSTGRES_PASSWORD=back -e POSTGRES_USER=back -p 9876:5432 -d postgres:14.8-alpine
 
@@ -21,12 +19,21 @@ migratedown:
 mock:
 	mockgen -package mockdb -destination project/mock/store.go github.com/gistGitUser/course/project/sqlc Store
 
+test-coverage:
+	go clean -testcache
+	go test -coverprofile cover.out ./...
+
+test-coverage-view:
+	go clean -testcache
+	go test -coverprofile coverage.out ./...
+	go tool cover -html=coverage.out
 
 sqlc:
 	sqlc generate
 
 test:
-	 go test -v -cover ./...
+	go clean -testcache
+	go test -v -cover ./...
 
 server:
 	cd project; go run main.go
--- project/api/account_test.go
+++ project/api/account_test.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	mockdb "github.com/gistGitUser/course/project/mock"
 	db "github.com/gistGitUser/course/project/sqlc"
 	"github.com/gistGitUser/course/project/util"
@@ -29,6 +30,7 @@ func TestGetAccountAPI(t *testing.T) {
 			name:      "OK",
 			accountID: account.ID,
 			buildStubs: func(store *mockdb.MockStore) {
+				//EXPECT returns an object that allows the caller to indicate expected use.
 				store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account.ID)).
 					Times(1).Return(account, nil)
 			},
@@ -78,7 +80,8 @@ func TestGetAccountAPI(t *testing.T) {
 	for i := range testCases {
 
 		tc := testCases[i]
-
+		//t.Run запускается в отдельной гоуртине, поэтому нужно делать
+		//костыль который показан сверху
 		t.Run(tc.name, func(t *testing.T) {
 			ctrl := gomock.NewController(t)
 			defer ctrl.Finish()
@@ -87,7 +90,8 @@ func TestGetAccountAPI(t *testing.T) {
 			tc.buildStubs(store)
 
 			//start test server
-
+			//сюда мы передаём наше хранилище, где будем
+			//вызывать наши функции
 			server := NewServer(store)
 			recorder := httptest.NewRecorder()
 
@@ -101,6 +105,232 @@ func TestGetAccountAPI(t *testing.T) {
 			tc.checkResponse(t, recorder)
 		})
 
+	}
+
+}
+
+func TestCreateAccountApi(t *testing.T) {
+	account := randomAccount()
+
+	account.Balance = 0
+
+	testCases := []struct {
+		name         string
+		body         gin.H
+		buildStubs   func(store *mockdb.MockStore)
+		expectStatus int
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"owner":    account.Owner,
+				"currency": account.Currency,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := db.CreateAccountsParams{
+					Owner:    account.Owner,
+					Currency: account.Currency,
+					Balance:  0,
+				}
+				//EXPECT returns an object that allows the caller to indicate expected use.
+				store.EXPECT().CreateAccounts(gomock.Any(), gomock.Eq(arg)).
+					Times(1).Return(account, nil)
+			},
+			expectStatus: http.StatusOK,
+		},
+		{
+
+			name: "InvalidCurrency",
+			body: gin.H{
+				"owner":    account.Owner,
+				"currency": "invalid",
+			},
+			//разобраться почему это так работает, т.к. по идее
+			//тут должна вернуться ошибка а не пустое значение
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().CreateAccounts(gomock.Any(), gomock.Any()).
+					//exact number of times a function call is expected to be executed.
+
+					Times(0)
+			},
+			expectStatus: http.StatusBadRequest,
+		},
+		{
+			name: "InvalidOwner",
+			body: gin.H{
+				"owner":    "",
+				"currency": account.Currency,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateAccounts(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			expectStatus: http.StatusBadRequest,
+		},
+		{
+			name: "InternalError",
+			body: gin.H{
+				"owner":    account.Owner,
+				"currency": account.Currency,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().CreateAccounts(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(db.Account{}, sql.ErrConnDone)
+			},
+			expectStatus: http.StatusInternalServerError,
+		},
+	}
+
+	for i := range testCases {
+
+		tc := testCases[i]
+		//t.Run запускается в отдельной гоуртине, поэтому нужно делать
+		//костыль который показан сверху
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			url := fmt.Sprintf("/accounts")
+
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+			req, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, req)
+
+			require.Equal(t, tc.expectStatus, recorder.Code)
+
+		})
+	}
+
+}
+
+func TestListAccountApi(t *testing.T) {
+
+	n := 5
+	accounts := make([]db.Account, n)
+	for i := 0; i < n; i++ {
+		accounts[i] = randomAccount()
+	}
+
+	type Query struct {
+		pageID   int
+		pageSize int
+	}
+
+	testCases := []struct {
+		name          string
+		query         Query
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(recoder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			query: Query{
+				pageID:   1,
+				pageSize: n,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := db.ListAccountsParams{
+					Limit:  int32(n),
+					Offset: 0,
+				}
+				//EXPECT returns an object that allows the caller to indicate expected use.
+				store.EXPECT().ListAccounts(gomock.Any(), gomock.Eq(arg)).
+					Times(1).
+					Return(accounts, nil)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchAccounts(t, recorder.Body, accounts)
+			},
+		},
+		{
+			name: "InvalidPageID",
+			query: Query{
+				pageID:   -1,
+				pageSize: n,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				//EXPECT returns an object that allows the caller to indicate expected use.
+				store.EXPECT().ListAccounts(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name: "InvalidPageSize",
+			query: Query{
+				pageID:   1,
+				pageSize: 11,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				//EXPECT returns an object that allows the caller to indicate expected use.
+				store.EXPECT().ListAccounts(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name: "InternalError",
+			query: Query{
+				pageID:   1,
+				pageSize: n,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					ListAccounts(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return([]db.Account{}, sql.ErrConnDone)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+
+		tc := testCases[i]
+		//t.Run запускается в отдельной гоуртине, поэтому нужно делать
+		//костыль который показан сверху
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			url := fmt.Sprintf("/accounts")
+
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
+
+			// Add query parameters to request URL
+			q := request.URL.Query()
+			q.Add("page_id", fmt.Sprintf("%d", tc.query.pageID))
+			q.Add("page_size", fmt.Sprintf("%d", tc.query.pageSize))
+			request.URL.RawQuery = q.Encode()
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(recorder)
+		})
 	}
 
 }
@@ -124,4 +354,14 @@ func requireBodyMatchAccount(t *testing.T, body *bytes.Buffer, account db.Accoun
 	require.NoError(t, err)
 	require.Equal(t, account, gotAccount)
 
+}
+
+func requireBodyMatchAccounts(t *testing.T, body *bytes.Buffer, accounts []db.Account) {
+	data, err := io.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotAccounts []db.Account
+	err = json.Unmarshal(data, &gotAccounts)
+	require.NoError(t, err)
+	require.Equal(t, accounts, gotAccounts)
 }
