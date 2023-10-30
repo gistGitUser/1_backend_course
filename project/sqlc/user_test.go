ede61a226354b37e62b9d52d2af2961ad7e20805
FIRST_NAME LAST_NAME <gistuser@bk.ru>
2023-10-30 20:28:00 +0300

    17 les

--- project/sqlc/user_test.go
+++ project/sqlc/user_test.go
@@ -9,9 +9,12 @@ import (
 )
 
 func CreateRandomUser(t *testing.T) User {
+
+	hashedPassword, err := util.HashPassword(util.RandomString(6))
+
 	arg := CreateUserParams{
 		Username:       util.RandomOwner(),
-		HashedPassword: "secret",
+		HashedPassword: hashedPassword,
 		FullName:       util.RandomOwner(),
 		Email:          util.RandomEmail(),
 	}
---
+++ project/test_curl/create_user.curl.sh
@@ -0,0 +1,2 @@
+curl -X POST localhost:8080/users -d \
+ '{"username": "quang2", "full_name": "Some Name",  "email": "emailfnweo@mail.com", "password":"secret"}'
