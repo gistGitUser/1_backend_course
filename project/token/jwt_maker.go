027a0717cc021bf0e909840300a14495d8ae94c1
FIRST_NAME LAST_NAME <gistuser@bk.ru>
2023-12-04 23:25:59 +0300

    add half 20

---
+++ project/token/jwt_maker.go
@@ -0,0 +1,56 @@
+package token
+
+import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+const minSecretKeySize = 32
+
+var ErrInvalidKeySize = fmt.Errorf("invalid key size: must be at least %d characters", minSecretKeySize)
+
+type JWTMaker struct {
+	secretKey string
+}
+
+func NewJWTMaker(secretKey string) (Maker, error) {
+	if len(secretKey) < minSecretKeySize {
+		return nil, ErrInvalidKeySize
+	}
+	return &JWTMaker{secretKey: secretKey}, nil
+}
+
+func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	return jwtToken.SignedString([]byte(maker.secretKey))
+}
+
+func (maker *JWTMaker) VerifyToken(token string) (*Payload, error) {
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, ErrInvalidToken
+		}
+
+		return []byte(maker.secretKey), nil
+	}
+
+	jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, keyFunc)
+	if err != nil {
+		//Кажется сейчас лучше так сделать
+		//можно и полный текст выдать, а не левую
+		//ошибку про jwt invalid token
+		return nil, err
+	}
+
+	payload, ok := jwtToken.Claims.(*Payload)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+	return payload, nil
+}
---
+++ project/token/payload.go
@@ -0,0 +1,53 @@
+package token
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"time"
+)
+
+var ErrExpiredToken = errors.New("token has expired")
+var ErrInvalidToken = errors.New("token is invalid")
+
+type Payload struct {
+	Username  string    `json:"username"`
+	ID        uuid.UUID `json:"id"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+	//чтобы можно было использовать нужно встроить это поле
+	jwt.RegisteredClaims
+}
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	//в целом можно было бы использовать дефолтные значения
+	//но лучше сразу указать как использовать кастомные поля
+	return &Payload{
+		Username: username,
+		ID:       tokenID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now()),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}, err
+
+	//return &jwt.MapClaims{
+	//	"username":   username,
+	//	"id":         tokenID,
+	//	"issued_at":  time.Now(),
+	//	"expired_at": time.Now().Add(duration),
+	//}, nil
+
+}
+
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
