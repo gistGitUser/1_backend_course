bc960daf42c4c4307994a748bf486dea9eec96e7
some user <user.19933@bk.ru>
2024-01-15 17:38:49 +0300

    start less 22

---
+++ project/api/middleware.go
@@ -0,0 +1,53 @@
+package api
+
+import (
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gistGitUser/course/project/token"
+	"net/http"
+	"strings"
+)
+
+const (
+	authHeaderKey  = "authorization"
+	authTypeBearer = "bearer"
+	authPayloadKey = "authorization_payload"
+)
+
+func authMiddleWare(maker *token.PasetoMaker) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader(authHeaderKey)
+		if len(authHeader) == 0 {
+			err := errors.New("authorization header isn't provided")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		fields := strings.Fields(authHeader)
+
+		if len(fields) < 2 {
+			err := errors.New("invalid auth header format")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		authType := strings.ToLower(fields[0])
+		if authType != authTypeBearer {
+			err := fmt.Errorf("unsupported authorization type %s", authType)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		accessToken := fields[1]
+		payload, err := maker.VerifyToken(accessToken)
+		if authType != authTypeBearer {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		ctx.Set(authPayloadKey, payload)
+		ctx.Next()
+
+	}
+}
---
+++ project/api/middleware_test.go
@@ -0,0 +1,46 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gistGitUser/course/project/token"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestAuthMiddleware(t *testing.T) {
+	testCases := []struct {
+		name          string
+		setupAuth     func(t *testing.T, request *http.Request, tokenMaker *token.PasetoMaker)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		//10:27
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			server := newTestServer(t, nil)
+
+			authPath := "/auth"
+
+			server.router.GET(
+				authPath,
+				authMiddleWare(server.tokenMaker),
+				func(ctx *gin.Context) {
+					ctx.JSON(http.StatusOK, gin.H{})
+				},
+			)
+
+			recorder := httptest.NewRecorder()
+			request, err := http.NewRequest(http.MethodGet, authPath, nil)
+			require.NoError(t, err)
+
+			tc.setupAuth(t, request, server.tokenMaker)
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(t, recorder)
+		})
+	}
+}
